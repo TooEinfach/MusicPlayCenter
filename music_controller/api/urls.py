152d863ba3a879958e4cd519955bd6f5b7b87a3e
Nick <whiteshadow148@gmail.com>
2021-03-24 16:59:56 -0600

    Finished T7
    Created room page that displays info

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Roomveiw, CreateRoomView
+from .views import Roomveiw, CreateRoomView, GetRoom
 
 urlpatterns = [
     path('home', Roomveiw.as_view()),
     path('create-room', CreateRoomView.as_view()),
+    path('get-room', GetRoom.as_view())
 ]
