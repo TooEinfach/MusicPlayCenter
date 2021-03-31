96ed07b0ad0184234b7a8a937e67d027b43fff4f
nick <whiteshadow148@gmail.com>
2021-03-31 15:46:18 -0600

    Typo and spacing issues

--- music_controller/spotify/util.py
+++ music_controller/spotify/util.py
@@ -2,10 +2,15 @@ from .models import SpotifyToken
 from django.utils import timezone
 from datetime import timedelta
 from .credentials import CLIENT_ID, CLIENT_SECRET
-from requests import post
+from requests import post, put, get
+
+
+BASE_URL = "https://api.spotify.com/v1/me/"
+
 
 def get_user_tokens(session_id):
     user_tokens = SpotifyToken.objects.filter(user=session_id)
+    print(user_tokens)
     if user_tokens.exists():
         return user_tokens[0]
     else:
@@ -21,10 +26,11 @@ def update_or_create_user_tokens(session_id, access_token, token_type, expires_i
         tokens.refresh_token = refresh_token
         tokens.expires_in = expires_in
         tokens.token_type = token_type
-        tokens.save(update_fields=['access_token', 'refresh_token', 'expires_in', 'token_type'])
-
+        tokens.save(update_fields=['access_token',
+                                   'refresh_token', 'expires_in', 'token_type'])
     else:
-        tokens = SpotifyToken(user=session_id, refresh_token=refresh_token, token_type=token_type)
+        tokens = SpotifyToken(user=session_id, access_token=access_token,
+                              refresh_token=refresh_token, token_type=token_type, expires_in=expires_in)
         tokens.save()
 
 
@@ -39,13 +45,14 @@ def is_spotify_authenticated(session_id):
 
     return False
 
+
 def refresh_spotify_token(session_id):
     refresh_token = get_user_tokens(session_id).refresh_token
 
     response = post('https://accounts.spotify.com/api/token', data={
         'grant_type': 'refresh_token',
         'refresh_token': refresh_token,
-        'client': CLIENT_ID,
+        'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET
     }).json()
 
@@ -54,4 +61,5 @@ def refresh_spotify_token(session_id):
     expires_in = response.get('expires_in')
     refresh_token = response.get('refresh_token')
 
-    update_or_create_user_tokens(session_id, access_token, token_type, expires_in, refresh_token)
+    update_or_create_user_tokens(
+        session_id, access_token, token_type, expires_in, refresh_token)
