313662bc7fef5b00a184401d6b9f094bf3294173
nick <whiteshadow148@gmail.com>
2021-03-31 14:59:39 -0600

    Typos in view.py and room.js

--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -4,10 +4,12 @@ from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
-from .util import update_or_create_user_tokens, is_spotify_authenticated
+from .util import update_or_create_user_tokens, is_spotify_authenticated, get_user_tokens
+from api.models import Room
+
 
 class AuthURL(APIView):
-    def get(self, request, format=None):
+    def get(self, request, fornat=None):
         scopes = 'user-read-playback-state user-modify-playback-state user-read-currently-playing'
 
         url = Request('GET', 'https://accounts.spotify.com/authorize', params={
@@ -18,6 +20,7 @@ class AuthURL(APIView):
         }).prepare().url
 
         return Response({'url': url}, status=status.HTTP_200_OK)
+
 
 def spotify_callback(request, format=None):
     code = request.GET.get('code')
@@ -36,15 +39,18 @@ def spotify_callback(request, format=None):
     refresh_token = response.get('refresh_token')
     expires_in = response.get('expires_in')
     error = response.get('error')
-    
+
     if not request.session.exists(request.session.session_key):
         request.session.create()
 
-    update_or_create_user_tokens(request.session.session_key, access_token, token_type, expires_in, refresh_token)
+    update_or_create_user_tokens(
+        request.session.session_key, access_token, token_type, expires_in, refresh_token)
 
     return redirect('frontend:')
 
+
 class IsAuthenticated(APIView):
     def get(self, request, format=None):
-        is_authenticated = is_spotify_authenticated(self.request.session.sessionkey)
+        is_authenticated = is_spotify_authenticated(
+            self.request.session.session_key)
         return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
